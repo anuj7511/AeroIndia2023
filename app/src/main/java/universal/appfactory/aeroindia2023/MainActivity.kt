3bb7a1d1fecfb2fe441d1a8d0304ad768eb4f740
Datshin1511 <datshinr2002@gmail.com>
2022-12-14 18:19:25 +0530

    Repository created on 14-12-2022

---
+++ app/src/main/java/universal/appfactory/aeroindia2023/MainActivity.kt
@@ -0,0 +1,13 @@
+package universal.appfactory.aeroindia2023
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import datshin.appfactory.aeroindia2023.R
+
+// Main Activity
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}

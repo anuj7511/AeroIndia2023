f1fd4cdd75959848f5229dc81b1ef06d02951e39
Datshin1511 <datshinr2002@gmail.com>
2022-12-16 23:46:46 +0530

    Timer set for managing layouts

--- app/src/main/java/universal/appfactory/aeroindia2023/MainActivity.kt
+++ app/src/main/java/universal/appfactory/aeroindia2023/MainActivity.kt
@@ -3,17 +3,27 @@ package universal.appfactory.aeroindia2023
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.os.CountDownTimer
 import android.util.Log
 import android.view.View
 import android.widget.EditText
 import android.widget.Toast
 import universal.appfactory.aeroindia2023.R
+import kotlin.concurrent.timerTask
 
 // Main Activity
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+
+        supportActionBar?.hide()
+        setContentView(R.layout.intro_layout)
+        val timer = object: CountDownTimer(6000, 1000) {
+            override fun onTick(millisUntilFinished: Long) {}
+
+            override fun onFinish() { setContentView(R.layout.activity_main) }
+        }
+        timer.start()
     }
 
     fun submitEmail(view: View){
@@ -24,16 +34,15 @@ class MainActivity : AppCompatActivity() {
 
 
         // TODO: OTP activity and API will be invoked to validate the given user ID using OTP
-        val intent = Intent(this, OtpActivity::class.java)
+        val intent = Intent(applicationContext, OtpActivity::class.java)
         intent.putExtra("email", email)
-        intent.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK)
+        intent.setFlags(Intent.FLAG_ACTIVITY_SINGLE_TOP)
         this.startActivity(intent)
 
         // TODO: New Activity invoked after validation
 
         // Sample code-chunk to test for input
-        Toast.makeText(this, "Email address: $email", Toast.LENGTH_LONG).show()
-        Log.e("User Info", "Name: $username\nMobile number: $mobileNo\nUsername: $username")
+        Log.i("User Info", "Name: $username\nMobile number: $mobileNo\nUsername: $username")
 
     }
 }

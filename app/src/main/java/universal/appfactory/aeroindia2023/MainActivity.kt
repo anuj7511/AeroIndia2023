4d649096c78865e56ff02fa187b56ef3453834dc
Datshin1511 <datshinr2002@gmail.com>
2022-12-16 08:55:27 +0530

    Layout created for register/login

--- app/src/main/java/universal/appfactory/aeroindia2023/MainActivity.kt
+++ app/src/main/java/universal/appfactory/aeroindia2023/MainActivity.kt
@@ -2,6 +2,9 @@ package universal.appfactory.aeroindia2023
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import android.widget.EditText
+import android.widget.Toast
 import datshin.appfactory.aeroindia2023.R
 
 // Main Activity
@@ -9,5 +12,10 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+    }
+
+    fun submitEmail(view: View){
+        val email = findViewById<EditText>(R.id.emailAddress).text
+        Toast.makeText(this, "Email address: $email", Toast.LENGTH_LONG).show()
     }
 }

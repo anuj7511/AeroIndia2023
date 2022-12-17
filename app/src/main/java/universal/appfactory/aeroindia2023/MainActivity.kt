28216ca2ac41e216c165a968534708986cf98e54
Datshin1511 <datshinr2002@gmail.com>
2022-12-17 17:02:59 +0530

    New intent activity completed

--- app/src/main/java/universal/appfactory/aeroindia2023/MainActivity.kt
+++ app/src/main/java/universal/appfactory/aeroindia2023/MainActivity.kt
@@ -6,10 +6,13 @@ import android.os.Bundle
 import android.os.CountDownTimer
 import android.util.Log
 import android.view.View
+import android.widget.Button
 import android.widget.EditText
+import android.widget.TextView
 import android.widget.Toast
 import universal.appfactory.aeroindia2023.R
 import kotlin.concurrent.timerTask
+
 
 // Main Activity
 class MainActivity : AppCompatActivity() {
@@ -17,32 +20,34 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
 
         supportActionBar?.hide()
+        // Displays intro layout or image
         setContentView(R.layout.intro_layout)
-        val timer = object: CountDownTimer(6000, 1000) {
+
+        // Timer to display the intro image
+        val timer = object: CountDownTimer(3000, 1000) {
             override fun onTick(millisUntilFinished: Long) {}
 
-            override fun onFinish() { setContentView(R.layout.activity_main) }
+            // Displays the Register activity (Main activity)
+            override fun onFinish() {
+                setContentView(R.layout.activity_main)
+                val button_id = findViewById<Button>(R.id.emailButton)
+
+                button_id.setOnClickListener{
+                    val email = findViewById<EditText>(R.id.emailAddress).text.toString()
+                    val username = findViewById<EditText>(R.id.username).text.toString()
+                    val mobileNo = findViewById<EditText>(R.id.mobileNumber).text.toString()
+
+                    Log.i("Unshared user information", "Name: $username\nMobile number: $mobileNo\nEmail: $email")
+
+                    val intent = Intent(this@MainActivity, OtpActivity::class.java)
+
+                    intent.putExtra("email", email)
+                    intent.putExtra("username", username)
+                    intent.putExtra("mobileNo", mobileNo)
+                    startActivity(intent)
+                }
+            }
         }
         timer.start()
-    }
-
-    fun submitEmail(view: View){
-        val email = findViewById<EditText>(R.id.emailAddress).text
-        val username = findViewById<EditText>(R.id.username).text
-        val mobileNo = findViewById<EditText>(R.id.mobileNumber).text
-
-
-
-        // TODO: OTP activity and API will be invoked to validate the given user ID using OTP
-        val intent = Intent(applicationContext, OtpActivity::class.java)
-        intent.putExtra("email", email)
-        intent.setFlags(Intent.FLAG_ACTIVITY_SINGLE_TOP)
-        this.startActivity(intent)
-
-        // TODO: New Activity invoked after validation
-
-        // Sample code-chunk to test for input
-        Log.i("User Info", "Name: $username\nMobile number: $mobileNo\nUsername: $username")
-
     }
 }
--- app/src/main/java/universal/appfactory/aeroindia2023/OtpActivity.kt
+++ app/src/main/java/universal/appfactory/aeroindia2023/OtpActivity.kt
@@ -4,27 +4,30 @@ import android.annotation.SuppressLint
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
 import android.widget.TextView
 import android.widget.Toast
 import universal.appfactory.aeroindia2023.R
 
 class OtpActivity : AppCompatActivity() {
 
-//    override fun onNewIntent(intent: Intent?) {
-//        super.onNewIntent(intent)
-//        val emailIDShared1: String = intent?.getStringExtra("email").toString()
-//    }
-
     @SuppressLint("SetTextI18n")
     override fun onCreate(savedInstanceState: Bundle?) {
 
+        supportActionBar?.hide()
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_otp)
 
-        val sharedEmailID: String = intent?.getStringExtra("email").toString()
-        findViewById<TextView>(R.id.userInfo).text = "Email: $sharedEmailID"
+        val sharedEmailID: String =  intent.getStringExtra("email").toString()
+        val sharedUsername: String = intent.getStringExtra("username").toString()
+        val sharedMobileNo: String = intent.getStringExtra("mobileNo").toString()
 
-        // TODO: OTP validation is required using API
+        // Userinfo echoed in logcat for reference
+        Log.i("Shared user information", "Name: $sharedUsername\nMobile number: $sharedMobileNo\nEmail: $sharedEmailID")
+
+        findViewById<TextView>(R.id.userInfo).text = " Name: $sharedUsername\nEmail: $sharedEmailID"
+
+        // TODO: OTP generation & validation is required using API
         Toast.makeText(this, "OTP Generated", Toast.LENGTH_LONG).show()
     }
 }

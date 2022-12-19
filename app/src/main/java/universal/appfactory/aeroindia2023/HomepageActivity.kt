9ca0b3a34624db47da2080e59edb21969d88f0f8
Datshin1511 <datshinr2002@gmail.com>
2022-12-19 14:38:38 +0530

    Homepage activity is created

--- app/src/main/java/universal/appfactory/aeroindia2023/HomepageActivity.kt
+++ app/src/main/java/universal/appfactory/aeroindia2023/HomepageActivity.kt
@@ -7,5 +7,9 @@ class HomepageActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_homepage)
+        supportActionBar?.hide()
+
+
+
     }
 }

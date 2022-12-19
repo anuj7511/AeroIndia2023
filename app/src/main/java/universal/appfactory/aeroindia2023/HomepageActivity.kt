d713877fa3b5261a9a14e2d71cac6906d10da1fe
Datshin1511 <datshinr2002@gmail.com>
2022-12-19 13:00:00 +0530

    Homepage layouts are created

---
+++ app/src/main/java/universal/appfactory/aeroindia2023/HomepageActivity.kt
@@ -0,0 +1,11 @@
+package universal.appfactory.aeroindia2023
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class HomepageActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_homepage)
+    }
+}

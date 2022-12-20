817603d0dc0d6d367d610243c9f440f51f085cd1
RS-Diksha <dikshabharti106@gmail.com>
2022-12-20 23:11:21 +0530

    Scaning part done

--- app/src/main/java/universal/appfactory/aeroindia2023/Feedback.kt
+++ app/src/main/java/universal/appfactory/aeroindia2023/Feedback.kt
@@ -1,10 +1,12 @@
 package universal.appfactory.aeroindia2023
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 
 import android.widget.Button
 import android.widget.EditText
 import android.widget.Toast
+import com.google.zxing.integration.android.IntentIntegrator
 import kotlinx.coroutines.*
 
 import retrofit2.Call
@@ -16,6 +18,9 @@ import universal.appfactory.aeroindia2023.databinding.ActivityFeedbackBinding
 class Feedback : AppCompatActivity() {
 
     private lateinit var binding: ActivityFeedbackBinding
+    private var qrScanIntegrator: IntentIntegrator? = null
+    lateinit var washroomId:String
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityFeedbackBinding.inflate(layoutInflater)
@@ -23,13 +28,46 @@ class Feedback : AppCompatActivity() {
         setContentView(view)
         val btn = findViewById<Button>(R.id.submit)
         val feedback = findViewById<EditText>(R.id.writeText)
-        btn.setOnClickListener { submitFeedback(feedback.text.toString()) }
+        setOnClickListener()
+        setupScanner()
+
+        btn.setOnClickListener { submitFeedback(feedback.text.toString(),washroomId) }
 
     }
 
+    private fun setupScanner() {
+        qrScanIntegrator = IntentIntegrator(this)
+    }
+
+    private fun setOnClickListener() {
+        binding.scanQrButton.setOnClickListener { performAction() }
+    }
+    private fun performAction() {
+        // Code to perform action when button is clicked.
+        qrScanIntegrator?.initiateScan()
+    }
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        val result = IntentIntegrator.parseActivityResult(requestCode, resultCode, data)
+        if (result != null) {
+            // If QRCode has no data.
+            if (result.contents == null) {
+                Toast.makeText(this, "Result Not Found", Toast.LENGTH_LONG).show()
+            } else {
+                   washroomId=result.contents
+
+                    // Data not in the expected format. So, whole object as toast message.
+                    Toast.makeText(this, result.contents, Toast.LENGTH_LONG).show()
+
+            }
+        } else {
+            super.onActivityResult(requestCode, resultCode, data)
+        }
+    }
+
+
     @OptIn(DelicateCoroutinesApi::class)
-    fun submitFeedback(feedback:String) {
-        val requestModel = RequestModel("123", "4586", "dfj", feedback)
+    fun submitFeedback(feedback:String,washroomId:String) {
+        val requestModel = RequestModel(washroomId, "4586", "dfj", feedback)
 
         val response = ServiceBuilder.buildService(ApiInterface::class.java)
         GlobalScope.launch(Dispatchers.IO + coroutineExceptionHandler) {

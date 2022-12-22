92d0619d6cf21d8c95fb5915d83838fb3067465a
RainaJain5 <rj35100@gmail.com>
2022-12-22 14:16:50 +0530

    Profile Activity

---
+++ app/src/main/java/universal/appfactory/aeroindia2023/ProfileActivity.kt
@@ -0,0 +1,11 @@
+package universal.appfactory.aeroindia2023
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class ProfileActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_profile)
+    }
+}

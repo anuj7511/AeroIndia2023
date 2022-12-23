12551fce43337b984e3286751e81c96a84bb4385
Datshin1511 <datshinr2002@gmail.com>
2022-12-23 16:33:20 +0530

    Dropdown menu refurbished

--- app/src/main/java/universal/appfactory/aeroindia2023/UserLoginActivity.kt
+++ app/src/main/java/universal/appfactory/aeroindia2023/UserLoginActivity.kt
@@ -7,6 +7,7 @@ import android.widget.ArrayAdapter
 import android.widget.AutoCompleteTextView
 import android.widget.Button
 import android.widget.EditText
+import android.widget.Spinner
 import androidx.core.content.ContentProviderCompat.requireContext
 import kotlinx.coroutines.currentCoroutineContext
 import universal.appfactory.aeroindia2023.databinding.ActivityUserLoginBinding
@@ -23,10 +24,13 @@ class UserLoginActivity : AppCompatActivity() {
         setContentView(R.layout.activity_user_login)
 
         // ArrayAdapter for user types
-        val user_types = resources.getStringArray(R.array.user_types)
-        val arrayAdapter = ArrayAdapter(this@UserLoginActivity, R.layout.user_type_dropdown_menu, user_types)
+        val spinner = findViewById<Spinner>(R.id.spinner)
 
-        findViewById<AutoCompleteTextView>(R.id.userTypeTextView).setAdapter(arrayAdapter)
+        val user_types = resources.getStringArray(R.array.user_types)
+        val arrayAdapter = ArrayAdapter<String>(this@UserLoginActivity, android.R.layout.simple_list_item_1, user_types)
+        arrayAdapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item)
+        spinner.setAdapter(arrayAdapter)
+
 
         val navigateBundle = Bundle()
         var intent = Intent(this@UserLoginActivity, OtpActivity::class.java)

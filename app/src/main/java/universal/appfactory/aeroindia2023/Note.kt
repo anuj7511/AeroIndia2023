c87e48756d3a1d76d55ae31c118a3f479681be28
RainaJain5 <rj35100@gmail.com>
2022-12-25 00:52:05 +0530

    Notes Implemented

---
+++ app/src/main/java/universal/appfactory/aeroindia2023/Note.kt
@@ -0,0 +1,15 @@
+package universal.appfactory.aeroindia2023
+
+import androidx.room.ColumnInfo
+import androidx.room.Entity
+import androidx.room.PrimaryKey
+import java.util.*
+
+@Entity(tableName = "notes")
+data class Note(
+    @PrimaryKey
+    @ColumnInfo(name = "dateAdded")
+    val dateAdded: Date,
+    @ColumnInfo(name = "noteText")
+    val noteText: String,
+)

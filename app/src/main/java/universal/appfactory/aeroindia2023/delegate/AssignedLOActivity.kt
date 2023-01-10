db82933c5160feea1b8edba80cec6c3f89d6aa8b
sarikimohan <sariki.mohankrishna@gmail.com>
2023-01-11 01:49:06 +0530

    Delegate changes commit

--- app/src/main/java/universal/appfactory/aeroindia2023/delegate/AssignedLOActivity.kt
+++ app/src/main/java/universal/appfactory/aeroindia2023/delegate/AssignedLOActivity.kt
@@ -3,9 +3,23 @@ package universal.appfactory.aeroindia2023.delegate
 import android.app.Application
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import android.widget.Toast
 import androidx.lifecycle.ViewModelProvider
 import kotlinx.android.synthetic.main.activity_assigned_loactivity.*
+import kotlinx.coroutines.DelicateCoroutinesApi
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.GlobalScope
+import kotlinx.coroutines.launch
+import retrofit2.Call
+import retrofit2.Callback
+import retrofit2.Response
+import universal.appfactory.aeroindia2023.ApiClient
+import universal.appfactory.aeroindia2023.ApiInterface
 import universal.appfactory.aeroindia2023.R
+import universal.appfactory.aeroindia2023.exhibitors.ExhibitorAdapter
+import universal.appfactory.aeroindia2023.exhibitors.ExhibitorModel
+import universal.appfactory.aeroindia2023.exhibitors.ExhibitorResponse
 
 class AssignedLOActivity : AppCompatActivity() {
     private lateinit var viewModel: DelegateViewModel
@@ -17,13 +31,44 @@ class AssignedLOActivity : AppCompatActivity() {
         viewModel = ViewModelProvider(this)[DelegateViewModel::class.java]
         viewModel.init((this as AppCompatActivity).applicationContext as Application)
 
-        viewModel.allDelegates.observe(this){
-            data = it as ArrayList<DelegateModel>
-            var item = data[0]
-            lo_name.text = item.getLiason_ooficer()
-            lo_gmail.text = item.getLO_email()
-            lo_contact_number.text  = item.getLO_mobile()
-            lo_organization.text = item.getLO_organisation()
+        fetchAssignedLoData()
+//        viewModel.allDelegates.observe(this){
+//            data = it as ArrayList<DelegateModel>
+//            var item = data[0]
+//            lo_name.text = item.getLiason_ooficer()
+//            lo_gmail.text = item.getLO_email()
+//            lo_contact_number.text  = item.getLO_mobile()
+//            lo_organization.text = item.getLO_organisation()
+//
+//        }
+    }
+
+    @OptIn(DelicateCoroutinesApi::class)
+    private fun fetchAssignedLoData() {
+        val delegateApi = ApiClient.getInstance().create(ApiInterface::class.java)
+
+        GlobalScope.launch(Dispatchers.IO) {
+
+            delegateApi.getDelegates("Bearer 61b25a411a2dad66bb7b6ff145db3c2f",1)?.enqueue(
+                object : Callback<DelegateResponse?>{
+                    override fun onResponse(
+                        call: Call<DelegateResponse?>,
+                        response: Response<DelegateResponse?>
+                    ) {
+                        data = response.body()?.data as ArrayList<DelegateModel>
+                        var item = data[0]
+                        lo_name.text = item.getLiason_ooficer()
+                        lo_gmail.text = item.getLO_email()
+                        lo_contact_number.text = item.getLO_mobile()
+                        lo_organization.text = item.getLO_organisation()
+                    }
+
+                    override fun onFailure(call: Call<DelegateResponse?>, t: Throwable) {
+                        Log.d("Delegate Failure Response: ", t.message.toString())
+                    }
+
+                }
+            )
 
         }
     }
--- app/src/main/java/universal/appfactory/aeroindia2023/delegate/DelegateVehicleActivity.kt
+++ app/src/main/java/universal/appfactory/aeroindia2023/delegate/DelegateVehicleActivity.kt
@@ -3,8 +3,19 @@ package universal.appfactory.aeroindia2023.delegate
 import android.app.Application
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
 import androidx.lifecycle.ViewModelProvider
+import kotlinx.android.synthetic.main.activity_assigned_loactivity.*
 import kotlinx.android.synthetic.main.vehicle_view.*
+import kotlinx.coroutines.DelicateCoroutinesApi
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.GlobalScope
+import kotlinx.coroutines.launch
+import retrofit2.Call
+import retrofit2.Callback
+import retrofit2.Response
+import universal.appfactory.aeroindia2023.ApiClient
+import universal.appfactory.aeroindia2023.ApiInterface
 import universal.appfactory.aeroindia2023.R
 
 class DelegateVehicleActivity : AppCompatActivity() {
@@ -17,15 +28,46 @@ class DelegateVehicleActivity : AppCompatActivity() {
         viewModel = ViewModelProvider(this)[DelegateViewModel::class.java]
         viewModel.init((this as AppCompatActivity).applicationContext as Application)
 
-        viewModel.allDelegates.observe(this){
-            data = it as ArrayList<DelegateModel>
-            var item = data[0]
-            Vehicle_number.text = item.getVehicle_number()
-            vehical_color.text = item.getVehicle_colour()
-            drive_name.text = item.getDriver_name()
-            driver_contact_number.text = item.getDriver_mobile_number()
+        fetchVehicleData()
+//        viewModel.allDelegates.observe(this){
+//            data = it as ArrayList<DelegateModel>
+//            var item = data[0]
+//            Vehicle_number.text = item.getVehicle_number()
+//            vehical_color.text = item.getVehicle_colour()
+//            drive_name.text = item.getDriver_name()
+//            driver_contact_number.text = item.getDriver_mobile_number()
+//        }
+
+
+    }
+
+    @OptIn(DelicateCoroutinesApi::class)
+    private fun fetchVehicleData() {
+        val delegateApi = ApiClient.getInstance().create(ApiInterface::class.java)
+
+        GlobalScope.launch(Dispatchers.IO) {
+
+            delegateApi.getDelegates("Bearer 61b25a411a2dad66bb7b6ff145db3c2f",1)?.enqueue(
+                object : Callback<DelegateResponse?> {
+                    override fun onResponse(
+                        call: Call<DelegateResponse?>,
+                        response: Response<DelegateResponse?>
+                    ) {
+                        data = response.body()?.data as ArrayList<DelegateModel>
+                        var item = data[0]
+                        Vehicle_number.text = item.getVehicle_number()
+                        vehical_color.text = item.getVehicle_colour()
+                        drive_name.text = item.getDriver_name()
+                        driver_contact_number.text = item.getDriver_mobile_number()
+                    }
+
+                    override fun onFailure(call: Call<DelegateResponse?>, t: Throwable) {
+                        Log.d("Delegate Failure Response: ", t.message.toString())
+                    }
+
+                }
+            )
+
         }
-
-
     }
 }

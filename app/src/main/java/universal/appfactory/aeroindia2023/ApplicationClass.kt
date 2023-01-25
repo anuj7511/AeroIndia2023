656e6b5b864e23743c9e7e6d7b58240fe6db0b0f
Datshin1511 <datshinr2002@gmail.com>
2023-01-25 15:03:26 +0530

    Application/Notification class corrected

--- app/src/main/java/universal/appfactory/aeroindia2023/ApplicationClass.kt
+++ app/src/main/java/universal/appfactory/aeroindia2023/ApplicationClass.kt
@@ -6,19 +6,32 @@ import com.onesignal.OneSignal
 
 const val ONESIGNAL_APP_ID = "1c53bac3-8c5c-4c3a-9cd2-38558dced0cf"
 
-class Notification: Application() {
+class ApplicationClass: Application() {
     override fun onCreate() {
         super.onCreate()
 
         Log.i("OneSignal Activity", "OneSignal notification class activated")
-        
+
         // Logging set to help debug issues, remove before releasing your app.
         OneSignal.setLogLevel(OneSignal.LOG_LEVEL.VERBOSE, OneSignal.LOG_LEVEL.NONE)
 
         // OneSignal Initialization
-        OneSignal.initWithContext(this)
         OneSignal.setAppId(ONESIGNAL_APP_ID)
 
-    }
+        // TODO: Requires modification
+        OneSignal.setNotificationWillShowInForegroundHandler {
+            val jsonObject = it.notification.additionalData
+            Log.i("JSON Object msg", jsonObject.toString())
+            it.complete(it.notification)
+        }
 
+        OneSignal.initWithContext(this@ApplicationClass)
+
+    }
 }
+
+// Used chunks of code
+
+//        OneSignal.startInit(this)
+//            .setNotificationReceivedHandler(ExampleNotificationReceivedHandler())
+//            .init()
